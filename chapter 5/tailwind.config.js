bf39149a8abbd338e80c0491844875d4ffb06d3c
wa-chira <wachiralatifah101@gmail.com>
2023-05-26 08:40:36 +0400

    initial commit

---
+++ chapter 5/tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./build/**/*.{html,js}'],
+  theme: {
+    extend: {
+      screens:{
+        'widescreen':{'raw': '(min-aspect-ratio: 3/2)'},
+        'tallscreen':{'raw': '(min-aspect-ratio: 13/20)'},
+
+      },
+      keyframes: {
+        'open-menu': {
+          '0%': {translate: 'scaleY(0)'},
+          '80%': {translate: 'scaleY(80)'},
+          '100%': {translate: 'scaleY(1)'},
+        },
+      },
+      animation: {
+        'open-menu': 'open-menu 0.5s ease-in-out forward'
+      }
+    },
+  },
+  plugins: [],
+}
+
